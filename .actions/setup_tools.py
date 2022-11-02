758685697158aeda329f3ce93ea38eb70816a7f4
Jirka Borovec <Borda@users.noreply.github.com>
2022-11-02 12:50:01 +0100

    docstring

--- .actions/setup_tools.py
+++ .actions/setup_tools.py
@@ -237,7 +237,7 @@ def create_mirror_package(src_folder: str, lit_pkg_mapping: dict) -> None:
 
 
 def distribute_version(src_folder: str, ver_file: str = "version.info") -> None:
-    """Replace the template date with today."""
+    """Copy the global version to all packages."""
     ls_ver = glob.glob(os.path.join(src_folder, "*", "__version__.py"))
     ver_template = os.path.join(src_folder, ver_file)
     for fpath in ls_ver:
